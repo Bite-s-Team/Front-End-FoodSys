bbea535de66a6582c45c9638e3df94b6bc27fe3a
Pedro Cardoso da Silva <pcsilvamaster@gmail.com>
2023-12-06 16:52:39 -0300

    feat: added preview home page

--- src/app/(application)/page.tsx
+++ src/app/(application)/page.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Image from "next/image";
 import { FoodCard } from "@/components/FoodCards/FoodCard";
 import image from "./image.jpeg";
 import { Button } from "@/components/ui/Button";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
 
 const data = [
   {
@@ -48,9 +50,15 @@ const data = [
   }
 ]
 
+import '@splidejs/react-splide/css';
+
+import '@splidejs/react-splide/css/sea-green';
+
+import '@splidejs/react-splide/css/core';
+
 export default function Home() {
   return (
-    <main>
+    <main className="font-sans">
       <section className="max-w-screen-2xl m-auto my-10">
         <h1 className="font-bold text-2xl text-zinc-700 my-4">Prepare para si</h1>
         <FoodCard.root>
@@ -71,43 +79,90 @@ export default function Home() {
           }
         </FoodCard.root>
       </section>
-      <div className="flex max-w-screen-2xl m-auto">
-        <Image
-          src={image} 
-          alt="Imagem do prato"
-          width={500}
-        />
+      <section className="flex max-w-screen-2xl m-auto items-center justify-between">
+        <div className="w-fit relative">
+          <h2 className="absolute bottom-20 drop-shadow-2xl text-center w-full z-10 text-white font-bold text-4xl">Escolha uma das opçoes na sua cidade</h2>
+          <Splide
+            hasTrack={true}
+            options={{
+              type: "loop",
+              perPage: 1,
+              drag: true,
+              gap: 8,
+              fixedHeight: 500,
+              fixedWidth: 800,
+              flickMaxPages: 1,
+              direction: "ttb",
+              height: 500,
+              width: 800,
+              paginationDirection: "rtl",
+              cover: true
+            }}
+          >
+            <SplideSlide>
+              <Image src={image} fill alt="teste" className="w-full" />
+            </SplideSlide>
+            <SplideSlide>
+              <Image src={image} fill alt="teste" className="w-full" />
+            </SplideSlide>
+            <SplideSlide>
+              <Image src={image} fill alt="teste" className="w-full" />
+            </SplideSlide>
+          </Splide>
+        </div>
+        <div className="flex flex-col gap-6">
+          <h2 className="font-semibold text-2xl flex items-center justify-center w-full text-orange-400 py-4 shadow-2xl bg-zinc-100 rounded-md">Recentemente Adicionados</h2>
+          <FoodCard.root>
+            <FoodCard.shadow>
+              <Image
+                src={image} 
+                alt="Imagem do prato"
+                className="w-full"
+              />
+              <FoodCard.details>
+                <FoodCard.title title="Barbecue"/>
+                <FoodCard.price title=" R$9,48"/>
+              </FoodCard.details>
+              <Button variant="cardButton">
+                Adicionar no carrinho
+              </Button>
+            </FoodCard.shadow>
+            <FoodCard.shadow>
+              <Image
+                src={image} 
+                alt="Imagem do prato"
+                className="w-full"
+              />
+              <FoodCard.details>
+                <FoodCard.title title="Barbecue"/>
+                <FoodCard.price title=" R$9,48"/>
+              </FoodCard.details>
+              <Button variant="cardButton">
+                Adicionar no carrinho
+              </Button>
+            </FoodCard.shadow>
+          </FoodCard.root>
+        </div>
+      </section>
+      <section className="max-w-screen-2xl m-auto my-10">
         <FoodCard.root>
-          <FoodCard.shadow>
-            <Image
-              src={image} 
-              alt="Imagem do prato"
-              className="w-full"
-            />
-            <FoodCard.details>
-              <FoodCard.title title="Barbecue"/>
-              <FoodCard.price title=" R$9,48"/>
-            </FoodCard.details>
-            <Button variant="cardButton">
-              Adicionar no carrinho
-            </Button>
-          </FoodCard.shadow>
-          <FoodCard.shadow>
-            <Image
-              src={image} 
-              alt="Imagem do prato"
-              className="w-full"
-            />
-            <FoodCard.details>
-              <FoodCard.title title="Barbecue"/>
-              <FoodCard.price title=" R$9,48"/>
-            </FoodCard.details>
-            <Button variant="cardButton">
-              Adicionar no carrinho
-            </Button>
-          </FoodCard.shadow>
+          { data.map((card) => (
+            <FoodCard.container key={card.id}>
+              <FoodCard.imageContainer>
+                <Image
+                  src={card.image} 
+                  alt="Imagem do prato"
+                  className="w-full rounded-md"
+                />
+              </FoodCard.imageContainer>
+              <FoodCard.title title={card.title}/>
+              <FoodCard.price title={card.price}/>
+            </FoodCard.container>
+          ))
+
+          }
         </FoodCard.root>
-      </div>
+      </section>
     </main>
   )
 }
